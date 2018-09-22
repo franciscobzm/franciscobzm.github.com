401d9f03a47fc9d48fbb8ccec6d3414b0d7b3a28
franciscobzm <42707663+franciscobzm@users.noreply.github.com>
2018-09-22 14:46:46 -0300

    initial

---
+++ 100% DEPORTE/js/main.js
@@ -0,0 +1,10 @@
+//alert("HOLA MUNDO JS");
+console.log("hola mundo en consola");
+//DOM (Document Object model)
+
+ let element = Document.queryselector(".container");
+
+ console.log(element);
+ 
+
+
